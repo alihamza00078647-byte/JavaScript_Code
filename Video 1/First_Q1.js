dfd9998a99e81a9489c728423fee98edb1befddc
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-05-01 10:48:56 +0500

    JS Code

---
+++ Video 1/First_Q1.js
@@ -0,0 +1,19 @@
+// const student = {
+//     fullName : "Aafam",
+//     Class : "2nd Year",
+//     age : "18",
+//     GPA : "3.4"
+// };
+
+
+// console.log(student)
+
+
+const product ={
+    productName: "pen",
+    color: "Black",
+    rating : "3.5",
+    price: "120 pkr"
+}
+
+console.log("Details about Product", product);
---
+++ Video 1/second_q2.js
@@ -0,0 +1,9 @@
+const profile = {
+    name : "Asta - San",
+    isFollow : true,
+    post : 125,
+    description : "To Educate Someone is Highest Privilege",
+}
+
+
+console.log("LinkedIn profile", profile)
