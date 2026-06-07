7d8e7773c74833bae922c98243ff0808640671ca
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-06-07 23:10:19 +0500

    JS Code

--- Video 5/Arrow_function.js
+++ Video 5/Arrow_function.js
@@ -1,17 +1,17 @@
 // Traditional Way of writing Function
-// function sum(x, y)
-// {
-//     console.log(x + y);
-// }
+function sum(x, y)
+{
+    console.log(x + y);
+}
 // function Call
-// sum(4,7);
+sum(4,7);
 
 
 //Modern 2015 JS Function Arrow function
 
-const sum = (a, b) => {
+const Sum = (a, b) => {
     console.log(a + b);
 }
 
-sum(3, 6);
+Sum(3, 6);
 
--- Video 5/Function.js
+++ Video 5/Function.js
@@ -1,13 +1,13 @@
 // Function Defination
 
 
-// function myfun(msg)    //Parameter -> msg
-// {
-//     console.log("Hello", msg);
-// }
+function myfun(msg)    //Parameter -> msg
+{
+    console.log("Hello", msg);
+}
 
-// n = prompt("Enter your Name: ");         This is also a Right Method
-// myfun(msg = n);                          // Argument -> Ali
+n = prompt("Enter your Name: ");         //This is also a Right Method
+myfun(msg = n);                          // Argument -> Ali
 
 
 
