109fcb45db23c6807880a5b4e2548a595b5d53f8
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-06-08 16:05:16 +0500

    JS Code

--- Video 5/Q2_forEach_loop.js
+++ Video 5/Q2_forEach_loop.js
@@ -2,6 +2,18 @@ let arrOfNumbers = [4, 5, 89, 99];
 
 
 // Pass Array to Sq Variable to console square
-arrOfNumbers.forEach((sq) => {
-    console.log(`Square of ${sq} is = ${sq*sq}`);
+arrOfNumbers.forEach((n) => {
+    console.log(`Square of ${n} is = ${n*n}`);
 });
+
+const newArray = arrOfNumbers.map(n => n * 3);
+// console.log(newArray)
+
+
+const filterArray = arrOfNumbers.filter(n => n%2===0);
+// console.log(filterArray)
+
+const reduceNum = arrOfNumbers.reduce((prev, next) => {
+    return prev + next
+});
+console.log(reduceNum)
