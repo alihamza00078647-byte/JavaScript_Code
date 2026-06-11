6fd2d4d366c2a972b8a6491894bd988a6bfe2ca8
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-06-11 21:53:21 +0500

    JS Code

--- Video 7/insertelements.js
+++ Video 7/insertelements.js
@@ -4,23 +4,23 @@ console.dir(el);
 
 // Append button at the end (inside)
 
-// let div = document.querySelector("div");
-// div.append(el);
+let div = document.querySelector("div");
+div.append(el);
 
 // Add button at start (inside)
 
-// let div = document.querySelector("div");
-// div.prepend(el);
+let div = document.querySelector("div");
+div.prepend(el);
 
 // add button at the start of node (outside)
 
-// let div = document.querySelector("div");
-// div.before(el);
+let div = document.querySelector("div");
+div.before(el);
 
 // add button at the end of node (outside)
 
-// let div = document.querySelector("div");
-// div.after(el);
+let div = document.querySelector("div");
+div.after(el);
 
 // Remove node from document
 // add button at the start of node (outside)
