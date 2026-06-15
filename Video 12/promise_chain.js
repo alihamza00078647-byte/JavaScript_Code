ee1d517702e8d9697b52f1103ddef34b048225aa
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-06-15 11:04:33 +0500

    JS Code

--- Video 12/promise_chain.js
+++ Video 12/promise_chain.js
@@ -17,26 +17,24 @@ function asyncFunc2(){
 }
 
 // console.log("Fetching data1....");
-// let p1 = asyncFunc1();
-// p1.then((res) => {
-//     console.log(res);
-// });
+let p1 = asyncFunc1();
+p1.then((res) => {
+    console.log(res);
+});
 
-// console.log("Fetching data2....");
-// let p2 = asyncFunc2();
-// p2.then((error) => {
-//     console.log(error);
-// });
+console.log("Fetching data2....");
+let p2 = asyncFunc2();
+p2.then((error) => {
+    console.log(error);
+});
 
 
 /* 2nd Method To resolve the Q */
 
 // Outer function
-// console.log("Fetching data1....");
-// asyncFunc1().then((res) => {
-//     // Inner Function
-//     console.log("Fetching data2....");
-//     asyncFunc2().then((error) => {});
-// });
-
-
+console.log("Fetching data1....");
+asyncFunc1().then((res) => {
+    // Inner Function
+    console.log("Fetching data2....");
+    asyncFunc2().then((error) => {});
+});
